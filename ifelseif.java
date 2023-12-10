5be2ceb963a939e8bc6ceded1ea97e2921466659
shivadarshini R <103628098+shivadarshinir@users.noreply.github.com>
2023-12-10 09:33:19 -0800

    Add files via upload
    ifelseif for practice

---
+++ ifelseif.java
@@ -0,0 +1,22 @@
+class HelloWorld
+ {
+    public static void main(String[] args)
+    {
+        int a = 5;
+        int b = 10;
+        
+        if(a<b)
+        {
+            System.out.println(a+" is less than "+b);
+        }
+        else if(a<b)
+        {
+            System.out.println(a+" is grater"+b);
+        }
+        else 
+        {
+           System.out.println(a+" is equal"+b); 
+        }
+        System.out.println("........................ ");
+    }
+ }
